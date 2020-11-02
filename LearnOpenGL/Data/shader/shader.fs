00660c30ad356408f5dd394ec9cd5459a47b3718
W <as5818956@gmail.com>
2020-11-02 22:39:00 +0900

    Texture

--- LearnOpenGL/Data/shader/shader.fs
+++ LearnOpenGL/Data/shader/shader.fs
@@ -2,8 +2,12 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    FragColor = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2);
 }
